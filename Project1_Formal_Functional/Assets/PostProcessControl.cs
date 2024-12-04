87eff816f15b8202cd3b3d3518b9f4c8b9665e8b
dexterdelandro <42786610+dexterdelandro@users.noreply.github.com>
2024-12-03 19:07:21 -0800

    added campfire and freeze screen effect

---
+++ Project1_Formal_Functional/Assets/PostProcessControl.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+
+
+public class PostProcessControl : MonoBehaviour
+{
+    public Volume postProcessingVolume;
+    public Vignette vignette;
+
+    public float intensityV;
+
+    public float freezeDuration = 3f;
+
+    public float changePerSecond = 0.1f;
+    public bool inFire = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        postProcessingVolume.profile.TryGet(out vignette);
+        vignette.intensity.value = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(vignette ==null)return;
+
+        if(!inFire && vignette.intensity.value<1){
+            vignette.intensity.value += changePerSecond * Time.deltaTime;
+            if(vignette.intensity.value==1){
+                Debug.Log("DIE");
+            }
+        } else if (inFire && vignette.intensity.value>0){
+            vignette.intensity.value -= changePerSecond * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Fire")){
+            Debug.Log("YAY");
+            inFire = true;
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.CompareTag("Fire")) inFire = false;
+    }
+}
